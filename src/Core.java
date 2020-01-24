192f11b2082fcd6515464230be98d301e7251bdd
KuronNazaki <akirahuynh2002@gmail.com>
2020-01-24 18:02:02 +0700

    Add some codes

--- src/Core.java
+++ src/Core.java
@@ -1,5 +1,15 @@
+// In this project, i'll learn the Java Core.
+
 public class Core {
     public static void main(String[] args) {
-        System.out.println("This line is added on GitHub");
+        System.out.println("Factorial of 20 is " + Factorial(20));
+    }
+
+    static public long Factorial (int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * Factorial(n - 1);
+        }
     }
 }
