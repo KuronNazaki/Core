f01218d795b85fe759b62f9a853b507eb0012f5b
KuronNazaki <akirahuynh2002@gmail.com>
2020-02-13 10:48:37 +0700

    Add some codes

--- src/Core.java
+++ src/Core.java
@@ -1,15 +1,41 @@
 // In this project, i'll learn the Java Core.
 
+import data.Ex;
+
 public class Core {
     public static void main(String[] args) {
-        System.out.println("Factorial of 20 is " + Factorial(20));
+        int[] arr = {5, 12, 34, 55, 66, 17, 18, 9, 12, 22, 15, 17};
+        QuickSort(arr, 0, arr.length - 1);
+        for (int x : arr) System.out.print(x + " ");
     }
 
-    static public long Factorial (int n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return n * Factorial(n - 1);
+    static void QuickSort (int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = Partition(arr, begin, end);
+
+            QuickSort(arr, begin, partitionIndex - 1);
+            QuickSort(arr, partitionIndex + 1, end);
         }
+    }
+
+    static int Partition (int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = begin - 1;
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i] ;
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = temp;
+
+        return i + 1;
     }
 }
---
+++ src/data/Ex.java
@@ -0,0 +1,80 @@
+package data;
+
+public class Ex {
+
+    static public long Factorial (int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    static public void HarmonicSum (int n) {
+        int maxDenominator = n;
+        double sumOfLeftToRight = 0.0;
+        double sumOfRightToLeft = 0.0;
+
+        for (int i = 1; i <= maxDenominator; i++) {
+            sumOfLeftToRight += (double) 1/i;
+        }
+        System.out.println("This is the sum from left to right " + sumOfLeftToRight);
+
+        for (int i = 5000; i >= 1; i--) {
+            sumOfRightToLeft += (double) 1/i;
+        }
+        System.out.println("This is the sum from right to left " + sumOfRightToLeft);
+
+        if (sumOfLeftToRight > sumOfRightToLeft) {
+            System.out.println("The sum from left to right is greater than the one from right to left");
+        } else {
+            System.out.println("The sum from right to left is greater than the one from left to right");
+        }
+    }
+
+    static public void ExtractNumber (int n) {
+        int number = n;
+        int howManyNum = 1;
+        int count = 0;
+        do {
+            howManyNum *= 10;
+            count++;
+        } while ((number / howManyNum) > 0);
+        int[] arrayOfNums = new int[count];
+        do {
+            int extractNum = n % 10;
+            arrayOfNums[count - 1] = extractNum;
+            n /= 10;
+            count--;
+        } while (n > 0);
+        for (int i : arrayOfNums) {
+            System.out.print(i + " ");
+        }
+    }
+
+//    public static void qsort (int[] list) {
+//        quicksort(list, 0, list.length - 1);
+//    }
+//
+//    private static void quicksort (int[] list, int left, int right) {
+//        int i, j;
+//        int x, y;
+//        i = left; j = right;
+//        x = list[(right + left) / 2]; // make a pivot
+//
+//        do {
+//            while (list[i] < x && i < right) i++;
+//            while (list[j] > x && j > left) j--;
+//
+//            if (i <= j) {
+//                y = list[i];
+//                list[i] = list[j];
+//                list[j] = y; // swap
+//                i++; j--;
+//            }
+//        } while (i <= j);
+//
+//        if (left < j) quicksort(list, left, j);
+//        if (right > i) quicksort(list, i, right);
+//    }
+//}
