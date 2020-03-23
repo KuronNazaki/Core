3864298c213166094050462aaa4c09c503cd1dde
KuronNazaki <akirahuynh2002@gmail.com>
2020-03-23 12:43:50 +0700

    Exercises

--- src/Core.java
+++ src/Core.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+
 // In this project, i'll learn the Java Core.
 
 // import data.Ex;
@@ -5,8 +8,19 @@
 // import java.util.Scanner;
 
 public class Core {
-    public static void main(String[] args) {
-        System.out.println("Hello world!"); // Thử pull cái này xuống xem sao
-    }
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a, b, c;
+
+        System.out.println("Input");
+        System.out.print("a = ");
+        a = scanner.nextDouble();
+        System.out.print("b = ");
+        b = scanner.nextDouble();
+        System.out.print("c = ");
+        c = scanner.nextDouble();
+        System.out.format("The average of three number : %.2f", (a + b + c) / 3);
+    } 
 
 }
